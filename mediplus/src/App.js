22552c8e50b2b8c689df5488e1997a2db81f8b40
jeswanth <jachu.k1212@gmail.com>
2024-10-28 22:18:12 +0530

    MedPlus

--- mediplus/src/App.js
+++ mediplus/src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Schedule from './Components/Schedule';
+import Slider from './Components/Slider';
+import Features from './Components/FeautesSection';
+import FunFacts from './Components/Fun-Facts';
+import WhyChoose from './Components/why-choose';
+import CallAction from './Components/Call-action';
+import Portfolio from './Components/Startportfolio';
+import Newsletter from './Components/Newsletter';
+import Appointment from './Components/Appointment';
+import Clients from './Components/clients';
+import Blog from './Components/Blog-Area';
+import Footers from './Components/Footer';
+import PricingTable from './Components/PricingTable';
+import Services from './Components/Service';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header/>
+     <Slider/>
+     <Schedule/>
+     <Features/>
+     <FunFacts/>
+     <WhyChoose/>
+     <CallAction/>
+     <Portfolio/>
+     <Services/>
+     <PricingTable/>
+     <Blog/>
+     <Clients/>
+     <Appointment/>
+     <Newsletter/>
+     <Footers/>
+    
+
     </div>
   );
 }
