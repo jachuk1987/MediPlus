a2f2776767c1103286630052c9f0a54ffcf6717e
jeswanth <jachu.k1212@gmail.com>
2024-11-12 13:08:03 +0530

    Medplus_Corrections

--- mediplus/src/App.js
+++ mediplus/src/App.js
@@ -1,43 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Header';
-import Schedule from './Components/Schedule';
-import Slider from './Components/Slider';
-import Features from './Components/FeautesSection';
-import FunFacts from './Components/Fun-Facts';
-import WhyChoose from './Components/why-choose';
-import CallAction from './Components/Call-action';
-import Portfolio from './Components/Startportfolio';
-import Newsletter from './Components/Newsletter';
-import Appointment from './Components/Appointment';
-import Clients from './Components/clients';
-import Blog from './Components/Blog-Area';
-import Footers from './Components/Footer';
-import PricingTable from './Components/PricingTable';
-import Services from './Components/Service';
+// import './App.css';
+import { Getprobutton } from './Components/Getprobutton';
+import { Headerarea } from './Components/Headerarea';
+import { Preloder } from './Components/Preloder';
+import { Pricingtable } from './Components/Pricingtable';
+import { Sliderarea } from './Components/Sliderarea';
+import { Startappoint } from './Components/Startappoint';
+import { Startblogarea } from './Components/Startblogarea';
+import { Startcalltoaction } from './Components/Startcalltoaction';
+import { Startclients } from './Components/Startclients';
+import { Startfeature } from './Components/Startfeature';
+import { Startfooter } from './Components/Startfooterarea';
+import { Startfortpolio } from './Components/Startfortpolio';
+import { Startfunfact } from './Components/Startfunfact';
+import { Startnewletterarea } from './Components/Startnewletterarea';
+import { Startservice } from './Components/Startservice';
+import { Startshudulearea } from './Components/Startshudulearea';
+import { Startwhychoose } from './Components/Startwhychoose';
 
 
 
 function App() {
   return (
     <div className="App">
-     <Header/>
-     <Slider/>
-     <Schedule/>
-     <Features/>
-     <FunFacts/>
-     <WhyChoose/>
-     <CallAction/>
-     <Portfolio/>
-     <Services/>
-     <PricingTable/>
-     <Blog/>
-     <Clients/>
-     <Appointment/>
-     <Newsletter/>
-     <Footers/>
-    
-
+      {/* <Preloder /> */}
+      <Getprobutton/>
+      <Headerarea/>
+      <Sliderarea/>
+      <Startshudulearea/>
+      <Startfeature/>
+      <Startfunfact/>
+      <Startwhychoose/>
+      <Startcalltoaction/>
+      <Startfortpolio/>
+      <Startservice/>
+      <Pricingtable/>
+      <Startblogarea/>
+      <Startclients/>
+      <Startappoint/>
+      <Startnewletterarea/>
+      <Startfooter/>
     </div>
   );
 }
